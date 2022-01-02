109f153e92199dbaa596e3dcb5d2de6f560ec1b9
Southball <jamie.choi.mail.2002@gmail.com>
2022-01-02 19:03:36 +0900

    🗃️ Add initial data into seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# rubocop:disable Lint/UselessAssignment
+
+technology_react = Technology.create! name: 'React',
+                                      slug: 'react',
+                                      icon_url: ActionController::Base.helpers.image_path('react.svg'),
+                                      url: 'https://reactjs.org/'
+
+technology_rails = Technology.create! name: 'Ruby on Rails',
+                                      slug: 'rails',
+                                      icon_url: ActionController::Base.helpers.image_path('rails.jpg'),
+                                      url: 'https://rubyonrails.org/'
+
+project_portfolio_rails = Project.create! title: 'Portfolio',
+                                          slug: 'portfolio-rails',
+                                          description: 'This portfolio website.',
+                                          technologies: [technology_rails]
+project_portfolio_rails.project_urls.create! title: 'GitHub',
+                                             url: 'https://github.com/southball/portfolio-rails'
+
+# rubocop:enable Lint/UselessAssignment
